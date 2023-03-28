815c06715b929e4a14fa91c9c3259bf3bab29013
Kim TaeSung <fopc4131@gmail.com>
2023-03-28 22:25:14 +0900

    gitignore 항목 추가 Memo.rtf

---
+++ KrHangMan-iOS/View/RankPage/RankListCell.swift
@@ -0,0 +1,8 @@
+//
+//  RankListCell.swift
+//  KrHangMan-iOS
+//
+//  Created by 김태성 on 2023/03/28.
+//
+
+import Foundation
---
+++ KrHangMan-iOS/View/RankPage/RankListView.swift
@@ -0,0 +1,8 @@
+//
+//  RankListView.swift
+//  KrHangMan-iOS
+//
+//  Created by 김태성 on 2023/03/28.
+//
+
+import Foundation
